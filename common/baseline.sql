60a54a8652c356ac94cc21b7ab1c2b4f933a43f2
YAMADA Kazuaki <k-yamada@mi-labo.co.jp>
2024-07-31 08:43:00 +0900

    add: files to introduce flyway to existing environment

---
+++ common/baseline.sql
@@ -0,0 +1,9 @@
+create table PRODUCT (
+    ID int not null,
+    NAME varchar(1000) not null,
+    CURRENT_PRICE int not null
+);
+create table CUSTOMER (
+    ID int not null,
+    NAME varchar(1000) not null
+);
