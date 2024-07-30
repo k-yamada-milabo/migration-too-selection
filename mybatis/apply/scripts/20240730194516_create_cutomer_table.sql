0bbb954abf1d18f0493f3948f99f1600ce54d4de
YAMADA Kazuaki <k-yamada@mi-labo.co.jp>
2024-07-30 20:08:58 +0900

    add: files to apply normally for MyBatis

---
+++ mybatis/apply/scripts/20240730194516_create_cutomer_table.sql
@@ -0,0 +1,12 @@
+-- // create cutomer table
+-- Migration SQL that makes the change goes here.
+create table CUSTOMER (
+    ID int not null,
+    NAME varchar(1000) not null
+)
+
+
+-- //@UNDO
+-- SQL to undo the change goes here.
+drop table CUSTOMER
+
