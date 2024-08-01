6263bb190d7c14a4e2ec311a96b4f3814e3b99c5
YAMADA Kazuaki <k-yamada@mi-labo.co.jp>
2024-08-01 12:52:58 +0900

    add: docker compose; flyway

---
+++ flyway/docker/migrations/V2__Create_seller_table.sql
@@ -0,0 +1,4 @@
+create table SELLER (
+    ID int not null,
+    NAME varchar(1000) not null
+)
---
+++ flyway/docker/migrations/V3__Create_itemlist_table.sql
@@ -0,0 +1,7 @@
+create table ITEM_LIST (
+    ID int not null,
+    CUSTOMER_ID int not null,
+    SELLER_ID int not null,
+    TOTAL_PRICE int not null,
+    TIMESTAMP timestamp not null
+)
